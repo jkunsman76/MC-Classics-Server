c14d1da0eee4f7bddb0541cdb37e4a868341cff5
jkuns76 <jkoonman@gmail.com>
2022-03-17 23:55:56 -0500

    im not sure what i changed...

--- mcclassicsapi/views/gear_head.py
+++ mcclassicsapi/views/gear_head.py
@@ -14,9 +14,11 @@ class ProfileView(ViewSet):
         gear_heads = GearHead.objects.all()
         serializer = GearHeadSerializer(gear_heads, many=True)
         return Response(serializer.data)
+    
     @action(methods=['get'], detail=False)
     def currentuser(self, request):
         """Gets the current user at http://localhost:8000/api/profiles/currentuser"""
         gear_head = GearHead.objects.get(user=request.auth.user)
         serializer = GearHeadSerializer(gear_head)
         return Response(serializer.data)
+    
