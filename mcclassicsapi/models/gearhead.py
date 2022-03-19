b2b7f73a0e0bbd3d74c49dfebd9eba6a166f5fbe
jkuns76 <jkoonman@gmail.com>
2022-03-19 01:55:58 -0500

    added new views for rendering the current logged in users helprequests and events, still trying to get the user edit working...

--- mcclassicsapi/models/gearhead.py
+++ mcclassicsapi/models/gearhead.py
@@ -9,4 +9,7 @@ class GearHead(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=1000)
     profile_img = models.ImageField(blank=True, null=True, upload_to='profilepics')
-   
+    # username = models.OneToOneField(User, on_delete=models.CASCADE, related_name="username")
+    # first_name = models.OneToOneField(User, on_delete=models.CASCADE related_name="first_name")
+    # last_name = models.OneToOneField(User, on_delete=models.CASCADE related_name="last_name")
+    # email = models.OneToOneField(User, on_delete=models.CASCADE related_name="email")
